e75fe69762d8838027434462b0f759132083d35f
Lharys Endon <lharysen@ox-h6.msk.21-school.ru>
2025-11-06 22:50:23 +0300

    edit 7.c

--- 7.c
+++ 7.c
@@ -1,5 +1,19 @@
-#include "postoev.c"
+#include "postoev.h"
 
 int main() {
-    char 
+    int n = 0;
+    printf("Введите кол-во команд: \n");
+    scanf("%d", &n);
+
+    int x = 0, y = 0;
+
+    printf("\n Начальная позиция: (%d, %d)\n\n", x, y);
+    printf("Шаг|Направление|Шагов|X|Y\n");
+    printf("---|-----------|-----|-|-\n");
+    
+
+
+
+    printf("%d %d %d", x, y, n);
+    return 0;
 }
